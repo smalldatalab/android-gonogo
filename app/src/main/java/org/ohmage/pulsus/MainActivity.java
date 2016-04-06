448d7f6452e1879f2947e5a2d47b092c57c4a08f
Anas Bouzoubaa <anas.bouzoubaa@gmail.com>
2016-04-06 11:33:00 -0400

    Initial commit (basic main layout - three buttons and toolbar)

---
+++ app/src/main/java/org/ohmage/pulsus/MainActivity.java
@@ -0,0 +1,19 @@
+package org.ohmage.pulsus;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set a toolbar to replace the action bar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
