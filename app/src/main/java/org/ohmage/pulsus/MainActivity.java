3b10178106491681aad5e3891e28eb3cdc38b21a
Anas Bouzoubaa <anas.bouzoubaa@gmail.com>
2016-04-06 15:26:49 -0400

    Action Toolbar with profile and logout actions

--- app/src/main/java/org/ohmage/pulsus/MainActivity.java
+++ app/src/main/java/org/ohmage/pulsus/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +19,29 @@ public class MainActivity extends AppCompatActivity {
         // Set a toolbar to replace the action bar.
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.items, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_profile:
+                Toast.makeText(getBaseContext(), "Clicked Profile", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.action_logout:
+                Toast.makeText(getBaseContext(), "Clicked Logout", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
