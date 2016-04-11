3cf53ad0da508d5170256abd3ea3ce5b0b40efaf
Anas Bouzoubaa <anas.bouzoubaa@gmail.com>
2016-04-11 18:50:47 -0400

    Added system toolbar instead of manually adding one

--- app/src/main/java/org/ohmage/pulsus/MainActivity.java
+++ app/src/main/java/org/ohmage/pulsus/MainActivity.java
@@ -16,10 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Set a toolbar to replace the action bar.
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
     @Override
