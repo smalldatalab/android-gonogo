9a47d3783c0fbad20d3ed59232f42572b3621d44
Anas Bouzoubaa <anas.bouzoubaa@gmail.com>
2016-05-23 12:51:54 -0400

    Completed Balloon Game

--- app/src/main/java/org/ohmage/pulsus/BalloonGame.java
+++ app/src/main/java/org/ohmage/pulsus/BalloonGame.java
@@ -1,21 +1,17 @@
 package org.ohmage.pulsus;
 
 import android.os.Handler;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
-import android.transition.Explode;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import tyrantgit.explosionfield.ExplosionAnimator;
+import java.util.Random;
+
 import tyrantgit.explosionfield.ExplosionField;
 
 public class BalloonGame extends AppCompatActivity {
@@ -25,8 +21,8 @@ public class BalloonGame extends AppCompatActivity {
     final float SCALE_X = 0.1f;
     final float SCALE_Y = 0.08f;
     final int MAX_PUMPS = 12;
-    final int numberOfBalloons = 15;
-    final float gainsPerPump = 0.25f;
+    final int NUMBER_OF_BALLOONS = 15;
+    final float gainPerPump = 0.25f;
 
     // UI
     ImageView balloonImageView;
@@ -35,10 +31,20 @@ public class BalloonGame extends AppCompatActivity {
     RelativeLayout instructionsLayout;
     RelativeLayout gameLayout;
     Button startButton;
+    TextView totalEarningsTextView;
+    TextView potentialGainTextView;
+    TextView instructionsTextView;
 
     // Test Variables
     private ExplosionField mExplosionField;
     int pumps = 0;
+    int currentBalloon = 1;
+    int numberOfExplosions = 0;
+    float earnings = 0.f;
+    float potentialGain = 0.f;
+
+    // Data Recording
+    Boolean lastBalloonExploded = false;
 
 
     @Override
@@ -96,37 +102,136 @@ public class BalloonGame extends AppCompatActivity {
         balloonImageView = (ImageView) findViewById(R.id.balloon_image_view);
         pumpButton = (Button) findViewById(R.id.pump_button);
         collectButton = (Button) findViewById(R.id.collect_button);
-    }
 
-    private void resetBalloon() {
-        balloonImageView.setScaleX(SCALE_ORIGINAL);
-        balloonImageView.setScaleY(SCALE_ORIGINAL);
-        balloonImageView.setAlpha(1.f);
-        mExplosionField.clear();
+        totalEarningsTextView = (TextView) findViewById(R.id.total_earnings_textview);
+        potentialGainTextView = (TextView) findViewById(R.id.potential_gain_textview);
+        instructionsTextView = (TextView) findViewById(R.id.instructions_textview);
     }
 
     private void tappedPump() {
 
-        if (pumps < MAX_PUMPS) {
-            balloonImageView.animate().scaleXBy(SCALE_X);
-            balloonImageView.animate().scaleYBy(SCALE_Y);
-            pumps++;
-
-        } else {
-            mExplosionField.explode(balloonImageView);
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    resetBalloon();
-                }
-            }, 1300);
-            pumps = 0;
+        // Prevent from pumping more than max number of balloons
+        if (currentBalloon > NUMBER_OF_BALLOONS) {
+            return;
         }
+
+        // Increase pumps
+        pumps++;
+
+        // Check whether it should implode now
+        if (shouldImplodeNow() || pumps >= MAX_PUMPS) {
+            implodeBalloon();
+            return;
+        }
+
+        // Disable pumping / collecting
+        enableButtons(false);
+
+        // Inflate Balloon
+        balloonImageView.animate().scaleXBy(SCALE_X);
+        balloonImageView.animate().scaleYBy(SCALE_Y).withEndAction(new Runnable() {
+            @Override
+            public void run() {
+                enableButtons(true);
+            }
+        });
+
+        potentialGain += gainPerPump;
+        updateEarningLabels();
     }
 
     private void tappedCollect() {
+        currentBalloon++;
+        lastBalloonExploded = false;
+        resetBalloon();
+    }
+
+    private void implodeBalloon() {
+
+        // Update trackers
+        potentialGain = 0;
+        currentBalloon++;
+        numberOfExplosions++;
+        lastBalloonExploded = true;
+        updateEarningLabels();
+
+        // Explosion animation
+        mExplosionField.explode(balloonImageView);
+        enableButtons(false);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                resetBalloon();
+                enableButtons(true);
+            }
+        }, 1300);
+    }
+
+    private Boolean shouldImplodeNow() {
+        if (pumps <= 1) {
+            return false;
+        }
+
+        // Probability of burst: 1-11, 1-10, 1-9, etc.
+        int maxValue = (int)(MAX_PUMPS-pumps+1);
+        Random r = new Random();
+        return (r.nextInt(maxValue) + 1) == 1;
+    }
+
+    private void updateEarningLabels() {
+        // If some balloons are left, show the count, else only show earnings
+        if (currentBalloon <= NUMBER_OF_BALLOONS) {
+            String s = "Balloon "+currentBalloon+" out of 15.\nTotal Earnings: $"+ String.format("%.2f", earnings);
+            totalEarningsTextView.setText(s);
+        } else {
+            String s = "Total Earnings:\n$"+ String.format("%.2f", earnings);
+            totalEarningsTextView.setText(s);
+        }
+        String s = "Potential Gain:\n$" + String.format("%.2f", potentialGain);
+        potentialGainTextView.setText(s);
+    }
+
+    private void enableButtons(Boolean enabled) {
+        pumpButton.setEnabled(enabled);
+        collectButton.setEnabled(enabled);
+    }
+
+    private void resetBalloon() {
+        // Reset Balloon Image
+        enableButtons(false);
+        balloonImageView.animate().scaleX(SCALE_ORIGINAL);
+        balloonImageView.animate().scaleY(SCALE_ORIGINAL).withEndAction(new Runnable() {
+            @Override
+            public void run() {
+                enableButtons(true);
+            }
+        });
+        balloonImageView.setAlpha(1.f);
+        mExplosionField.clear();
+
+        // Update trackers
+        earnings += potentialGain;
+        potentialGain = 0;
         pumps = 0;
-        balloonImageView.setScaleX(SCALE_ORIGINAL);
-        balloonImageView.setScaleY(SCALE_ORIGINAL);
+        updateEarningLabels();
+
+        if (currentBalloon > NUMBER_OF_BALLOONS) {
+            showResults();
+        }
+    }
+
+    private void showResults() {
+        // Disable buttons and hide game layout
+        enableButtons(false);
+        gameLayout.animate().alpha(0.f);
+        startButton.setAlpha(0.f);
+        instructionsLayout.bringToFront();
+
+        // Results
+        String results = "";
+        results += "Total Earnings: $" + String.format("%.2f", earnings) +"\n\n";
+        results += "Number of Balloon Explosions: "+ numberOfExplosions +"\n\n";
+        instructionsTextView.setText(results);
+        instructionsLayout.animate().alpha(1.f);
     }
 }
